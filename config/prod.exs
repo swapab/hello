86f63d54cfb2cfda177e93506008abf11b279d82
Swapnil Abnave <swapnil.abnave@hexad.de>
2018-05-18 12:17:22 +0200

    update manifest and prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -2,8 +2,7 @@ use Mix.Config
 
 config :hello, HelloWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "immense-anchorage-73738.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "https", host: System.get_env("CF_INSTANCE_GUID"), port: System.get_env("PORT")],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
@@ -14,5 +13,4 @@ config :logger, level: :info
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
